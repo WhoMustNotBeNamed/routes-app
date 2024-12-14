0a3cd6c161aaccb0a0888e9702954058bc9662e0
Alex <yevsyukovaleks@gmail.com>
2024-12-15 01:31:06 +0300

    Fix query select in UserCheckpointRepository

--- src/main/kotlin/ru/hse/db/routes_app/repository/UserCheckpointRepository.kt
+++ src/main/kotlin/ru/hse/db/routes_app/repository/UserCheckpointRepository.kt
@@ -1,11 +1,20 @@
 package ru.hse.db.routes_app.repository
 
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import ru.hse.db.routes_app.model.UserCheckpoint
+import java.util.*
 
 @Repository
 interface UserCheckpointRepository : CrudRepository<UserCheckpoint, String> {
 
-    fun findAllByRouteSessionId(routeSessionId: String): List<UserCheckpoint>
+    @Query(
+        """
+        select * 
+        from user_checkpoint 
+        where route_session_id = :routeSessionId
+        """
+    )
+    fun findAllByRouteSessionId(routeSessionId: UUID): List<UserCheckpoint>
 }
